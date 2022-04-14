1e881322fa989217b025acac2496e0ac3c602f22
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2022-04-14 19:35:38 +0300

    Добавление третьей практики

---
+++ Lesson3/intentapp/src/main/java/ru/mirea/danilov/intentapp/SecondActivity.java
@@ -0,0 +1,18 @@
+package ru.mirea.danilov.intentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        String date = getIntent().getStringExtra("date");
+        TextView text = findViewById(R.id.textView);
+        text.setText(date);
+    }
+}
