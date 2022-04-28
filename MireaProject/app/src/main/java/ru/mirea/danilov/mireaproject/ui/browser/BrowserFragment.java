04f5a622bfdf451d6d370f72f6e82c18777d6184
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2022-04-28 21:33:57 +0300

    Проект 7-й практики

--- MireaProject/app/src/main/java/ru/mirea/danilov/mireaproject/ui/browser/BrowserFragment.java
+++ MireaProject/app/src/main/java/ru/mirea/danilov/mireaproject/ui/browser/BrowserFragment.java
@@ -1,19 +1,29 @@
 package ru.mirea.danilov.mireaproject.ui.browser;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import ru.mirea.danilov.mireaproject.MainActivity;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import ru.mirea.danilov.mireaproject.R;
 import ru.mirea.danilov.mireaproject.databinding.FragmentBrowserBinding;
 
 public class BrowserFragment extends Fragment {
@@ -29,6 +39,10 @@ public class BrowserFragment extends Fragment {
         View root = binding.getRoot();
 
         final EditText searchBar = binding.searchBar;
+
+        SendHttpRequestTask task = new SendHttpRequestTask();
+        task.execute("https://i.aipserver.ru/Безымянный-файл-изображения-(30)-2.jpg");
+
         browserViewModel.getText().observe(getViewLifecycleOwner(), searchBar::setText);
 
         binding.searchButton.setOnClickListener(this::onSearchButtonClicked);
@@ -47,5 +61,28 @@ public class BrowserFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private class SendHttpRequestTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                return BitmapFactory.decodeStream(input);
+            }catch (Exception e){
+                Log.d(TAG,e.getMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            ImageView imageView = (ImageView) getActivity().findViewById(R.id.picture);
+            imageView.setImageBitmap(result);
+        }
     }
 }
